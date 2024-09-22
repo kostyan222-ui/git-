44d11c0488e5eca4038aa32270b401e5704a2932
USER PC1 <egorp1351@gmail.com>
2024-09-22 18:41:41 +0300

    added the function of calculating the factorial

--- dsgs.py
+++ dsgs.py
@@ -1,1 +1,12 @@
-#здесь бдует код
+#здесь будет код
+number = int(input('Введите число: '))
+factorial = 1
+
+while number > 1:
+    factorial = factorial * number
+    number = number - 1
+
+print(factorial)
+
+# Введите число: 10
+# 3628800
